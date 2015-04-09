d20b69467b4db5887bce1e2997d9fbfd5c5815db
Dylan O'Brennan <the.dob@gmail.com>
2015-04-08 22:24:57 -0500

    removed JSON support from models to clean them up

--- app/controllers/provinces_controller.rb
+++ app/controllers/provinces_controller.rb
@@ -2,13 +2,11 @@ class ProvincesController < ApplicationController
   before_action :set_province, only: [:show, :edit, :update, :destroy]
 
   # GET /provinces
-  # GET /provinces.json
   def index
     @provinces = Province.all
   end
 
   # GET /provinces/1
-  # GET /provinces/1.json
   def show
   end
 
@@ -26,38 +24,26 @@ class ProvincesController < ApplicationController
   def create
     @province = Province.new(province_params)
 
-    respond_to do |format|
-      if @province.save
-        format.html { redirect_to @province, notice: 'Province was successfully created.' }
-        format.json { render :show, status: :created, location: @province }
-      else
-        format.html { render :new }
-        format.json { render json: @province.errors, status: :unprocessable_entity }
-      end
+    if @province.save
+      redirect_to @province, notice: 'Province was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /provinces/1
-  # PATCH/PUT /provinces/1.json
   def update
-    respond_to do |format|
-      if @province.update(province_params)
-        format.html { redirect_to @province, notice: 'Province was successfully updated.' }
-        format.json { render :show, status: :ok, location: @province }
-      else
-        format.html { render :edit }
-        format.json { render json: @province.errors, status: :unprocessable_entity }
-      end
+    if @province.update(province_params)
+      redirect_to @province, notice: 'Province was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /provinces/1
-  # DELETE /provinces/1.json
   def destroy
     @province.destroy
-    respond_to do |format|
-      format.html { redirect_to provinces_url, notice: 'Province was successfully destroyed.' }
-      format.json { head :no_content }
+      redirect_to provinces_url, notice: 'Province was successfully destroyed.'
     end
   end
 
