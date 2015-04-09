3a35a5d952da5731d0babc66000fe97d164e3ef6
Dylan O'Brennan <the.dob@gmail.com>
2015-04-09 11:21:22 -0500

    added image support

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -55,6 +55,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :price, :stock_quantity, :created_at, :updated_at)
+      params.require(:product).permit(:name, :description, :size, :price, :stock_quantity, :image_filename, :created_at, :updated_at)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
-	validates :name, :description, :price, :size, :stock_quantity, presence: true
+	SIZES = [5.5, 6.0, 6.5, 7.0, 7.5, 8.0]
+
+	#validates :size, :inclusion => [5.5, 6.0, 6.5, 7.0, 7.5, 8.0]
+
+	validates :name, :description, :price, :stock_quantity, presence: true
 end
