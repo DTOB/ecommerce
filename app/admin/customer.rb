13c26b38efde759001f3f8fd497ebd8c13bd74c5
Dylan O'Brennan <the.dob@gmail.com>
2015-04-09 16:12:01 -0500

    Trying to work out ActiveAdmin... Cannot edit Customers properly as I cannot enter a province_id

--- app/admin/customer.rb
+++ app/admin/customer.rb
@@ -2,6 +2,31 @@ ActiveAdmin.register Customer do
 
   permit_params :first_name, :last_name, :address, :city, :country, :postal_code, :email, :province_id, :created_at, :updated_at
   
+  index do
+    column :id
+    column :first_name
+    column :last_name
+    column :address
+    column :city
+    column :country
+    column "Province" do |m|
+      usr = Province.find(m.province_id).name
+    end 
+    column :postal_code
+    column :email
+    column :created_at
+    column :updated_at
+  end
+    
+  form do |f|
+  f.semantic_errors # shows errors on :base
+  f.inputs          # builds an input field for every attribute
+  f.actions         # adds the 'Submit' and 'Cancel' buttons
+end
+
+
+
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ActiveRecord::Base
-	validates :first_name, :last_name, :email, presence: true
+	validates :first_name, :last_name, :email, :province_id, presence: true
+		
+	
 end
